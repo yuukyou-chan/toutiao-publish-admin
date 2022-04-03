e88f720745e90cb7427488ccee93e38a87c5098c
yuukyou-chan <1968159353@qq.com>
2022-04-03 21:39:51 +0800

    文章发布完

---
+++ src/api/image.js
@@ -0,0 +1,15 @@
+/**
+ * 新建文章
+ */
+
+import request from '@/utils/request'
+export const addArticle = (data, draft = false) => {
+  return request({
+    method: 'POST',
+    url: '/mp/v1_0/articles',
+    params: {
+      draft // 是否存为草稿（true 为草稿）
+    },
+    data
+  })
+}
--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -10,14 +10,24 @@
         <!-- /面包屑路径导航 -->
       </div>
   </div>
-    <el-form ref="form" :model="form" label-width="80px">
-  <el-form-item label="标题">
+    <el-form
+    ref="publish-form"
+    :model="article"
+    :rules="formRules"
+    label-width="80px" >
+  <el-form-item label="标题" prop="title">
     <el-input v-model="article.title"></el-input>
   </el-form-item>
-    <el-form-item label="内容">
-    <el-input type="textarea" v-model="article.content"></el-input>
+    <el-form-item label="内容" prop="content">
+    <!-- <el-input type="textarea" v-model="article.content"></el-input> -->
+     <el-tiptap
+     v-model="article.content"
+     :extensions="extensions"
+     lang='zh'
+     height="400"
+     />
   </el-form-item>
-    <el-form-item label="封面">
+    <el-form-item label="封面" >
     <el-radio-group v-model="article.cover.type">
       <el-radio :label="1">单图</el-radio>
       <el-radio :label="3">三图</el-radio>
@@ -25,7 +35,7 @@
       <el-radio :label="-1">自动</el-radio>
     </el-radio-group>
   </el-form-item>
-  <el-form-item label="频道">
+  <el-form-item label="频道" prop="channel_id">
     <el-select v-model="article.channel_id" labelplaceholder="请选择频道">
       <el-option
        :label="chanel.name"
@@ -43,7 +53,6 @@
 </el-card>
 
 </template>
-
 <script>
 import {
   getArticleChannels,
@@ -51,31 +60,90 @@ import {
   getArticle,
   updateArticle
 } from '@/api/article'
+import {
+  ElementTiptap,
+  Doc,
+  Text,
+  Paragraph,
+  Heading,
+  Bold,
+  Underline,
+  Italic,
+  Strike,
+  ListItem,
+  BulletList,
+  OrderedList,
+  Image
+} from 'element-tiptap'
+import 'element-tiptap/lib/index.css'
+import { uploadImage } from '@/api/image'
 export default {
   name: 'PublishIndex',
-  components: {},
+  components: {
+    'el-tiptap': ElementTiptap
+  },
   props: {},
   data () {
     return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
-      },
-      channels: [],
+      channels: [], // 文章频道列表
       article: {
-        title: '',
-        content: '',
-        cover: {
-          type: 0,
-          images: []
+        title: '', // 文章标题
+        content: '', // 文章内容
+        cover: { // 文章封面
+          type: 1, // 封面类型 -1:自动，0-无图，1-1张，3-3张
+          images: [] // 封面图片的地址
         },
         channel_id: null
+      },
+      // 编辑器的 extensions
+      // 它们将会按照你声明的顺序被添加到菜单栏和气泡菜单中
+      extensions: [
+        new Doc(),
+        new Text(),
+        new Paragraph(),
+        new Heading({ level: 5 }),
+        new Bold({ bubble: true }), // 在气泡菜单中渲染菜单按钮
+        new Underline({ bubble: true, menubar: false }), // 在气泡菜单而不在菜单栏中渲染菜单按钮
+        new Italic(),
+        new Strike(),
+        new ListItem(),
+        new BulletList(),
+        new OrderedList(),
+        new Image({
+          uploadrequest (file) {
+            const fd = new FormData()
+            fd.append('image', file)
+            uploadImage(fd).then(res => {
+              console.log(res)
+            })
+          }
+        }
+        )
+      ],
+      formRules: {
+        title: [
+          { required: true, message: '请输入文章标题', trigger: 'blur' },
+          { min: 5, max: 30, message: '长度在 5 到 30 个字符', trigger: 'blur' }
+        ],
+        content: [
+          // { required: true, message: '请输入文章内容', trigger: 'change' }
+          {
+            validator (rule, value, callback) {
+              console.log('content validator')
+              if (value === '<p></p>') {
+                // 验证失败
+                callback(new Error('请输入文章内容'))
+              } else {
+                // 验证通过
+                callback()
+              }
+            }
+          },
+          { required: true, message: '请输入文章内容', trigger: 'blur' }
+        ],
+        channel_id: [
+          { required: true, message: '请选择文章频道' }
+        ]
       }
     }
   },
@@ -83,7 +151,12 @@ export default {
   watch: {},
   created () {
     this.loadChannels()
-    this.loadArticle()
+    // 由于我们让发布和修改使用的同一个组件
+    // 所以这里要判断
+    // 如果路由路径参数中有 id，则请求展示文章内容
+    if (this.$route.query.id) {
+      this.loadArticle()
+    }
   },
   mounted () {},
   methods: {
@@ -93,28 +166,55 @@ export default {
       })
     },
     onPublish (draft = false) {
-      const articleId = this.$route.query.id
-      if (articleId) {
-        updateArticle(articleId, this.article, draft).then(res => {
-          this.$message({
-            message: '操作成功',
-            type: 'success'
+      this.$refs['publish-form'].validate(valid => {
+        // 验证失败，停止提交表单
+        if (!valid) {
+          return
+        }
+        // 验证通过，提交表单
+        // 找到数据接口
+        // 封装请求方法
+        // 请求提交表单
+        // 如果是修改文章，则执行修改操作，否则执行添加操作
+        const articleId = this.$route.query.id
+        if (articleId) {
+          // 执行修改操作
+          updateArticle(articleId, this.article, draft).then(res => {
+            console.log(res)
+            this.$message({
+              message: `${draft ? '存入草稿' : '发布'}成功`,
+              type: 'success'
+            })
+            // 跳转到内容管理页面
+            this.$router.push('/article')
           })
-        })
-      } else {
-        addArticle(this.article, draft).then(res => {
-          this.$message({
-            message: '发布成功',
-            type: 'success'
+        } else {
+          addArticle(this.article, draft).then(res => {
+            // 处理响应结果
+            // console.log(res)
+            this.$message({
+              message: `${draft ? '存入草稿' : '发布'}成功`,
+              type: 'success'
+            })
+            // 跳转到内容管理页面
+            this.$router.push('/article')
           })
-          this.$router.push('/article')
-        })
-      }
+        }
+      })
     },
+    // 修改文章：加载文章内容
     loadArticle () {
+      console.log('loadArticle')
+      // 找到数据接口
+      // 封装请求方法
+      // 请求获取数据
       getArticle(this.$route.query.id).then(res => {
+        // 模板绑定展示
         this.article = res.data.data
       })
+    },
+    onUpdateCover (index, url) {
+      this.article.cover.images[index] = url
     }
   }
 }
